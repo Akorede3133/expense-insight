528adc4617996d410937d4bb2d6de3aeb7d30e37
akorede <emiolasaheed1@gmail.com>
2023-11-21 21:33:01 +0100

    Implemented category creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  get 'categories/new'
-  get 'expenses/new'
-  root 'home#index'
+  root "home#index"
   devise_for :users, path: '', path_names: {
-    sing_in: 'login',
+    sign_in: 'login',
     sign_out: 'logout'
   }, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
+
+  resources :categories, only: [:index, :new, :create] do
+    resources :expenses, only: [:index, :new, :create]
+  end
 end
