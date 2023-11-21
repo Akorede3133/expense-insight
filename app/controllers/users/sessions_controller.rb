b5ca4e1b9eab77fa94fffe3eb7009b67b2ef28bd
akorede <emiolasaheed1@gmail.com>
2023-11-21 21:36:36 +0100

    Redirect too category page upon sign in

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
@@ -19,6 +17,10 @@ class Users::SessionsController < Devise::SessionsController
   # end
 
   # protected
+
+  def after_sign_in_path_for(_resource)
+    categories_path
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
