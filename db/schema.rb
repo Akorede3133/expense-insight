ba7554ab6a52b6662a039a4f5c348ea669d02a7d
akorede <emiolasaheed1@gmail.com>
2023-11-21 21:44:23 +0100

    Fix assocaition problem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_21_200050) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_21_204103) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -46,6 +46,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_21_200050) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
   create_table "category_expenses", force: :cascade do |t|
@@ -81,6 +83,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_21_200050) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "categories", "users"
   add_foreign_key "category_expenses", "categories"
   add_foreign_key "category_expenses", "expenses"
   add_foreign_key "expenses", "users", column: "author_id"
